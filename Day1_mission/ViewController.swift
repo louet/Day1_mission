9fb3453d364d2e519c1b76e1a054fd328a79c1c7
louet <kygenesis@gmail.com>
2015-10-30 09:06:58 +0900

    1일차 미션

--- Day1_mission/ViewController.swift
+++ Day1_mission/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mPhoto: UIImageView!
+    
+    var count : Int = 1
+    @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var btnBack: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setBtn(count)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,31 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func back(sender: AnyObject) {
+        changeImage("back")
+    }
+    
+    @IBAction func next(sender: AnyObject) {
+        changeImage("next")
+    }
+    
+    func changeImage(action: String){
+        count = (action == "back") ? count-1 : count+1
+        mPhoto.image = UIImage(named: "\(count).jpg")
+        setBtn(count)
+    }
+    
+    func setBtn(index: Int){
+        if index <= 1 {
+            btnBack.hidden = true
+        }
+        else if index >= 7 {
+            btnNext.hidden = true
+        }
+        else {
+            btnBack.hidden = false
+            btnNext.hidden = false
+        }
+    }
 }
 
